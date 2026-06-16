a075d890b0c3b556ad131deb3966dd529aca2a52
Igor Rafael <igorcoelho808@gmail.com>
2026-06-15 22:22:33 -0300

    Feat: Finalização da estrutura para envio de emails.

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,20 +1,34 @@
 package com.ms.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 
 @Component
 public class EmailConsumer {
 	
+	final EmailService service;
+	
+	public EmailConsumer(EmailService service) {
+		this.service = service;
+	}
+	
 	@RabbitListener(queues = "${broker.queue.email.name}")
 	public void listenEmailQueue(@Payload EmailRecordDto emailRecord) {
 		
-		System.out.println(emailRecord.emailTo());
-		System.out.println(emailRecord.subject());
-		System.out.println(emailRecord.text());
+		var emailModel = new EmailModel();
+		BeanUtils.copyProperties(emailRecord, emailModel);
+		service.sendEmail(emailModel);
+		
+		//Test-Terminal
+		//System.out.println(emailRecord.emailTo());
+		//System.out.println(emailRecord.subject());
+		//System.out.println(emailRecord.text());
 	}
 
 }
--- email/src/main/java/com/ms/email/services/EmailService.java
+++ email/src/main/java/com/ms/email/services/EmailService.java
@@ -1,8 +1,54 @@
 package com.ms.email.services;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class EmailService {
+	
+	final EmailRepository repository;
+	
+	final JavaMailSender emailSender;
+	
+	public EmailService(EmailRepository repository, JavaMailSender emailSender) {
+		this.repository = repository;
+		this.emailSender = emailSender;
+	}
+	
+	@Value(value = "${spring.mail.username}")
+	private String emailFrom;
+	
+	@Transactional
+	public EmailModel sendEmail(EmailModel model) {
+		try {
+		model.setSendDateEmail(LocalDateTime.now());
+		model.setEmailFrom(emailFrom);
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(model.getEmailTo());
+		message.setSubject(model.getSubject());
+		message.setText(model.getText());
+		emailSender.send(message);
+		
+		model.setStatusEmail(StatusEmail.SENT);
+			
+		} catch (Exception e) {
+			model.setStatusEmail(StatusEmail.ERROR);
+		
+		}finally {
+			return repository.save(model);
+		}
+		
+	}
 
 }
